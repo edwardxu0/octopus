dd0de240b745b8f28ddf08e2334484c945a3bc76
Edward <edward@xu.null>
2022-04-19 01:02:33 -0400

    minor

--- study/s2.py
+++ study/s2.py
@@ -2,7 +2,7 @@ import numpy as np
 
 sleep_time = 5
 train_nodes = None
-train_nodes_ex = 'affogato12,affogato13,affogato14,cheetah01,lynx08,lynx09,lynx11,lynx12,ai04,ai02,affogato15'
+train_nodes_ex = 'affogato12,affogato13,affogato14,cheetah01,lynx08,lynx09,lynx10,lynx11,lynx12,ai04,ai02,affogato15'
 
 veri_nodes = ['doppio'+x for x in ['01', '02', '03', '04', '05']]  # ,'06','07','08','09','10']]
 veri_nodes_ex = None
@@ -12,7 +12,7 @@ networks = {'NetS': [128]*3,
             'NetM': [1024]*3,
             'NetL': [1024]*6}
 
-seeds = [*range(3)]
+seeds = [*range(5)]
 
 heuristics = {'bias_shaping': {
     'mode': 'standard',
