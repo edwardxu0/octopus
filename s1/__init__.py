6ad1cae8f7fb780df131c511000647b31a16820b
Your Name <you@example.com>
2023-09-30 16:16:18 +0800

    new bs on all layers

--- s1/__init__.py
+++ s1/__init__.py
@@ -1,8 +1,11 @@
 import numpy as np
 
+platform = "slurm"
 sleep_train = 0
 sleep_verify = 0
-platform = "slurm"
+nb_train_nodes = 8
+train_partition = "gpu"
+# train_partition = "gnolim"
 
 # train_nodes = ["cheetah01"]  # change octopus+ L:267
 # train_nodes_ex = "ai01,ai07,ai08,lynx07,lynx08,lynx09,lynx10,sds01,sds02,lotus,titanx01,titanx02,titanx03,titanx04,titanx05,titanx06,lynx11,lynx12,affogato11,affogato12,affogato15,adriatic01,adriatic02,adriatic03,adriatic04,adriatic05,adriatic06,ristretto02,ristretto03,ristretto04,cheetah01,jaguar01,jaguar02,jaguar03"
